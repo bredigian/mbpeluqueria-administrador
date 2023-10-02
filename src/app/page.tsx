2d18f796c0c5c36e6acc4aeb2ff0396a2ecce5f6
Gianluca Bredice - Nitro 5 <bredicegian@gmail.com>
2023-10-02 20:07:41 -0300

    [feature] add modal for signout confirmation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,22 @@
 import Button from "@/components/Button"
 import Image from "next/image"
 import Menu from "@/components/Menu"
+import Modal from "@/components/Modal"
 import Subtitle from "@/components/Subtitle"
 import Title from "@/components/Title"
 import logo from "@/assets/images/logo.jpg"
 import { toast } from "sonner"
 import { useAuthStore } from "@/store/auth"
+import { useState } from "react"
 
 const Home = () => {
   const { user, token, signOut } = useAuthStore()
+
+  const [showModal, setShowModal] = useState(false)
+
+  const handleModal = () => {
+    setShowModal(!showModal)
+  }
 
   const onSignOut = async () => {
     try {
@@ -21,26 +29,41 @@ const Home = () => {
   }
 
   return (
-    <main className="flex flex-col items-center gap-8 py-16 px-6">
-      <Image
-        width={160}
-        height={160}
-        src={logo}
-        alt="Logo de MB Peluqueria"
-        style={{
-          borderRadius: "100%",
-        }}
-      />
-      <div className="flex flex-col gap-2 ml-2">
-        <Title>{`¡Hola, ${user.name.split(" ")[0]}!`}</Title>
-        <Subtitle>
-          En este menú podrás seleccionar la opcion de lo que desees gestionar
-        </Subtitle>
+    <main>
+      <div className="flex flex-col items-center gap-8 py-16 px-6">
+        <Image
+          width={160}
+          height={160}
+          src={logo}
+          alt="Logo de MB Peluqueria"
+          style={{
+            borderRadius: "100%",
+          }}
+        />
+        <div className="flex flex-col gap-2 ml-2">
+          <Title>{`¡Hola, ${user.name.split(" ")[0]}!`}</Title>
+          <Subtitle>
+            En este menú podrás seleccionar la opcion de lo que desees gestionar
+          </Subtitle>
+        </div>
+        <Menu />
+        <Button onClick={handleModal} type="button" style="fixed bottom-6">
+          Cerrar sesión
+        </Button>
       </div>
-      <Menu />
-      <Button onClick={onSignOut} type="button" style="fixed bottom-6">
-        Cerrar sesión
-      </Button>
+      {showModal && (
+        <Modal>
+          <div className="bg-dark-regular flex flex-col items-center gap-4 p-8 w-[300px] rounded-[55px]">
+            <Title style="text-[#ffffff70]">¿Estás seguro?</Title>
+            <Button onClick={onSignOut} type="button" style="w-[140px] mt-2">
+              Cerrar sesión
+            </Button>
+            <Button onClick={handleModal} type="button" style="w-[140px]">
+              Cancelar
+            </Button>
+          </div>
+        </Modal>
+      )}
     </main>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,24 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
         dark: {
           light: "#2C2A2F50",
           regular: "#37353A",
           bold: "#2C2A2F",
+          "bold-transparent": "#2C2A2F95",
         },
         white: {
           "extra-light": "#ffffff35",
