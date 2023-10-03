f95c5c76eb9f1e8518306aa2090dd0a04c500f5c
Gianluca Bredice - Nitro 5 <bredicegian@gmail.com>
2023-10-03 00:12:47 -0300

    [feature] past shifts are disables

--- src/components/ShiftItem.tsx
+++ src/components/ShiftItem.tsx
@@ -14,21 +14,39 @@ const ShiftItem = ({
 }) => {
   const isLongName = data.user.name.length > 16
 
+  const hour = parseInt(data?.hour.hour.split(":")[0])
+  const minutes = parseInt(data?.hour.hour.split(":")[1])
+  const isPast =
+    Date.now() >
+    new Date(
+      data?.day.year,
+      data?.day.month,
+      data?.day.day,
+      hour,
+      minutes
+    ).getTime()
+
   return (
     <div
-      onClick={handleActive}
-      className={`bg-dark-bold flex flex-col items-center gap-4 w-full py-8 px-10 rounded-[33px] duration-200 ease-in-out ${
+      onClick={!isPast ? handleActive : undefined}
+      className={`${
+        !isPast ? "bg-dark-bold" : "bg-dark-light"
+      } flex flex-col items-center gap-4 w-full py-8 px-10 rounded-[33px] duration-200 ease-in-out ${
         !isActive ? "h-[90px]" : "h-[180px]"
       } overflow-hidden`}
     >
       <div className="flex items-center justify-between w-full">
-        <span className="text-yellow-regular font-bold text-xl">
+        <span
+          className={`${
+            !isPast ? "text-yellow-regular" : "text-yellow-light"
+          } font-bold text-xl`}
+        >
           {data.hour.hour}
         </span>
         <h5
-          className={`text-yellow-regular font-semibold ${
-            !isLongName ? "text-base" : "text-sm"
-          }`}
+          className={`${
+            !isPast ? "text-yellow-regular" : "text-yellow-light"
+          } font-semibold ${!isLongName ? "text-base" : "text-sm"}`}
         >
           {data.user.name}
         </h5>
