1456e0d05bc9def9ecb49e30db12041ae0c3ec72
Gianluca Bredice - Nitro 5 <bredicegian@gmail.com>
2023-10-06 16:52:09 -0300

    [feature] add whatsapp button for send shift recorder to client

--- src/components/ShiftItem.tsx
+++ src/components/ShiftItem.tsx
@@ -1,3 +1,4 @@
+import { BsWhatsapp } from "react-icons/bs"
 import Button from "./Button"
 import { Summary } from "@/types/summary.types"
 import { motion } from "framer-motion"
@@ -40,7 +41,7 @@ const ShiftItem = ({
           ? "bg-dark-bold cursor-pointer"
           : "bg-dark-light cursor-not-allowed"
       } flex flex-col items-center gap-4 w-full py-8 px-10 rounded-[33px] duration-200 ease-in-out ${
-        !isActive ? "h-[90px]" : "h-[180px]"
+        !isActive ? "h-[90px]" : "h-[200px]"
       } overflow-hidden`}
     >
       <div className="flex items-center justify-between w-full">
@@ -73,15 +74,31 @@ const ShiftItem = ({
           {data.user.phone}
         </a>
       </div>
-      <Button
-        onClick={handleModal}
-        style={`bg-dark-regular text-xs ${
-          !isActive ? "invisible opacity-0" : "visible opacity-100"
-        } duration-200 ease-in-out`}
-        type="button"
-      >
-        Cancelar
-      </Button>
+      <div className="flex items-center justify-between w-full mt-4">
+        <Button
+          onClick={handleModal}
+          style={`bg-dark-regular text-[#ffffff60] text-xs ${
+            !isActive ? "invisible opacity-0" : "visible opacity-100"
+          } duration-200 ease-in-out`}
+          type="button"
+        >
+          Cancelar
+        </Button>
+        <a
+          href={`https://api.whatsapp.com/send?phone=${data.user.phone}&text=¡Hola%20👋!%0ATe%20recuerdo%20que%20tenés%20un%20turno%20el%20día%20*${data.day.dateString}*%20a%20las%20*${data.hour.hour}hs*%0ASi%20deseás%20cancelar%20el%20turno,%20podrás%20solicitarlo%20desde%20la%20página%20de%20turnos.%0A¡Muchas%20gracias,%20te%20espero!%20💈`}
+          target="_blank"
+        >
+          <Button
+            style={`flex items-center gap-2 bg-white-semi-light text-dark-bold text-xs ${
+              !isActive ? "invisible opacity-0" : "visible opacity-100"
+            } duration-200 ease-in-out`}
+            type="button"
+          >
+            <BsWhatsapp className="text-base" />
+            Recordar
+          </Button>
+        </a>
+      </div>
     </motion.div>
   )
 }
